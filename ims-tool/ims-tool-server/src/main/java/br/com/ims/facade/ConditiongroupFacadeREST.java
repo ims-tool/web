4d5bbea2de01d44f19b88e0874ef102d6f44332c
Cesar Yanaka <cyanaka@gmail.com>
2015-08-13 23:37:18 -0300

    Finalizado primeira parte de facades serviço rest.

---
+++ ims-tool/ims-tool-server/src/main/java/br/com/ims/facade/ConditiongroupFacadeREST.java
@@ -0,0 +1,83 @@
+package br.com.ims.facade;
+
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import br.com.ims.tool.entity.Conditiongroup;
+
+@Stateless
+@Path("conditiongroup")
+public class ConditiongroupFacadeREST extends AbstractFacade<Conditiongroup> {
+    @PersistenceContext(unitName = "ivrPersistence")
+    private EntityManager em;
+
+    public ConditiongroupFacadeREST() {
+        super(Conditiongroup.class);
+    }
+
+    @POST
+    @Override
+    @Consumes({"application/xml", "application/json"})
+    public void create(Conditiongroup entity) {
+        super.create(entity);
+    }
+
+    @PUT
+    @Path("{id}")
+    @Consumes({"application/xml", "application/json"})
+    public void edit(@PathParam("id") Integer id, Conditiongroup entity) {
+        super.edit(entity);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void remove(@PathParam("id") Integer id) {
+        super.remove(super.find(id));
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces({"application/xml", "application/json"})
+    public Conditiongroup find(@PathParam("id") Integer id) {
+        return super.find(id);
+    }
+
+    @GET
+    @Override
+    @Produces({"application/xml", "application/json"})
+    public List<Conditiongroup> findAll() {
+        return super.findAll();
+    }
+
+    @GET
+    @Path("{from}/{to}")
+    @Produces({"application/xml", "application/json"})
+    public List<Conditiongroup> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
+        return super.findRange(new int[]{from, to});
+    }
+
+    @GET
+    @Path("count")
+    @Produces("text/plain")
+    public String countREST() {
+        return String.valueOf(super.count());
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+    
+}
---
+++ ims-tool/ims-tool-server/src/main/java/br/com/ims/facade/ConditionmapFacadeREST.java
@@ -0,0 +1,85 @@
+package br.com.ims.facade;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import br.com.ims.tool.entity.Conditionmap;
+
+/**
+ *
+ * @author Cesar
+ */
+@Stateless
+@Path("conditionmap")
+public class ConditionmapFacadeREST extends AbstractFacade<Conditionmap> {
+    @PersistenceContext(unitName = "ivrPersistence")
+    private EntityManager em;
+
+    public ConditionmapFacadeREST() {
+        super(Conditionmap.class);
+    }
+
+    @POST
+    @Override
+    @Consumes({"application/xml", "application/json"})
+    public void create(Conditionmap entity) {
+        super.create(entity);
+    }
+
+    @PUT
+    @Path("{id}")
+    @Consumes({"application/xml", "application/json"})
+    public void edit(@PathParam("id") String id, Conditionmap entity) {
+        super.edit(entity);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void remove(@PathParam("id") String id) {
+        super.remove(super.find(id));
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces({"application/xml", "application/json"})
+    public Conditionmap find(@PathParam("id") String id) {
+        return super.find(id);
+    }
+
+    @GET
+    @Override
+    @Produces({"application/xml", "application/json"})
+    public List<Conditionmap> findAll() {
+        return super.findAll();
+    }
+
+    @GET
+    @Path("{from}/{to}")
+    @Produces({"application/xml", "application/json"})
+    public List<Conditionmap> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
+        return super.findRange(new int[]{from, to});
+    }
+
+    @GET
+    @Path("count")
+    @Produces("text/plain")
+    public String countREST() {
+        return String.valueOf(super.count());
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+    
+}
