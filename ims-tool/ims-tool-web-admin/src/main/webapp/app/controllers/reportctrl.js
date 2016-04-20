e9c2243fe3049fb45bda561dece7d744dbd4cd3b
yanaka <cyanaka@gmail.com>
2016-04-19 23:45:14 -0300

    Finalizado tela de controlpanel.

--- ims-tool/ims-tool-web-admin/src/main/webapp/app/controllers/reportctrl.js
+++ ims-tool/ims-tool-web-admin/src/main/webapp/app/controllers/reportctrl.js
@@ -4,9 +4,57 @@ app.controller('ReportCtrl', function($rootScope, $location, $scope, $http, $mdD
 
 					$scope.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
 					$rootScope.activetab = $location.path();
+					$scope.report = {};
+					$http.get('http://'+window.location.hostname+':8080/ims-tool-server/rest/report/getTypeControlPanel').success(function(data1) {
+						$scope.data = {
+							    repeatSelect: null,
+							    availableOptions: data1,
+						};
+					});
 					
-
-
+					$scope.getControlPanelList = function(){
+						
+						$http.get('http://'+window.location.hostname+':8080/ims-tool-server/rest/report/getControlPanelList/'+$scope.report.type).success(function(data1) {
+							$scope.controlPanelList = data1;
+						});
+					}
+					
+					$scope.changeFlag = function(index){
+						var data = $scope.controlPanelList[index];
+						var statusOld = "";
+						data.loginid = localStorage.getItem("login");
+						bootbox.confirm("Deseja alterar o status?", function(result) {
+							if(result === true){
+								saveControlPanel(data);
+								console.log(data);
+								if(data.status === 'true'){
+									statusOld = 'false'
+								}else{
+									statusOld = 'true';
+								}
+								setLog(1, "update flag controlPanel ims-tool-web-admin", "webreport", statusOld, 0, data.id);
+							}else{
+								$http.get('http://'+window.location.hostname+':8080/ims-tool-server/rest/report/getControlPanelList/'+$scope.report.type).success(function(data1) {
+									$scope.controlPanelList = data1;
+								});
+							}
+						});
+					}
+					$scope.changeTimeout = function(index){
+						var data = $scope.controlPanelList[index];
+						data.loginid = localStorage.getItem("login");
+						bootbox.confirm("Deseja alterar timeout?", function(result) {
+							if(result === true){
+								saveControlPanel(data);
+								console.log(data);
+								setLog(1, "update timeout controlPanel ims-tool-web-admin", "webreport", data.timeout.toString(), 0, data.id);
+							}else{
+								$http.get('http://'+window.location.hostname+':8080/ims-tool-server/rest/report/getControlPanelList/'+$scope.report.type).success(function(data1) {
+									$scope.controlPanelList = data1;
+								});
+							}
+						});
+					}
 				})
 
 function setLog(ptypeid, pdescription, partifact, poriginalvalue, partifactid, pvalueid){
@@ -18,4 +66,16 @@ function setLog(ptypeid, pdescription, partifact, poriginalvalue, partifactid, p
 		contentType : "application/json",
 		dataType : 'json'
 	});
+}
+
+function saveControlPanel(controlPanel){
+	
+	$.ajax({
+		type : "POST",
+		data : JSON.stringify(controlPanel),
+		url : 'http://'+window.location.hostname+":8080/ims-tool-server/rest/report/updateControlPanel",
+		contentType : "application/json",
+		dataType : 'json'
+	});
+	
 }
